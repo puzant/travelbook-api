793119ad38e7aed411aeee9a036096107c0b33a3
Puzant <PBAKJEJIAN@hotmail.com>
2024-01-23 20:37:55 +0200

    :truck: rename schema

--- models/adventure.js
+++ models/adventure.js
@@ -2,16 +2,16 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
 
-const AdventureEntrySchema = new Schema({
+const AdventureSchema = new Schema({
   date: { type: Date },
   duration: { type: String },
-  country: { type: Stirng },
-  city: { type: Stirng },
+  country: { type: String },
+  city: { type: String },
   travelStyle: { 
     type: String,
 		enum: ['Adventure Travel', 'Cultural Travel', 'Luxury Travel', 'Budget Travel']
   }
 })
 
-const AdventureEntry = mongoose.model('AdventureEntry', AdventureEntrySchema)
-module.exports = AdventureEntry;
+const Adventure = mongoose.model('Adventure', AdventureSchema)
+module.exports = Adventure;
