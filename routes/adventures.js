919b2616dbc85946fa34653731ba4fe1a3f29c93
Puzant <PBAKJEJIAN@hotmail.com>
2024-01-25 12:40:21 +0200

    🤡 add mock data

--- routes/adventures.js
+++ routes/adventures.js
@@ -1,8 +1,35 @@
 const express = require('express')
+const Adventure = require('../models/adventure'); 
+
 const router = express.Router()
 
-router.get('/', (req, res) => {
-	res.send('list of all adventures')
+const mockdata = [
+	{
+		date: '2022-9/2',
+		duration: '16',
+		country: 'Armenia',
+		city: 'Yerevan',
+		travelStyle: 'Adventure Travel'
+	},
+	{
+		date: '2015-5/10',
+		duration: '10',
+		country: 'Lebanon',
+		city: 'Beirut',
+		travelStyle: 'Adventure Travel'
+	},
+]
+
+router.get('/', async (req, res) => {
+	res.render('index', { adventures: mockdata });
+})
+
+router.get('/add-adventure', async (req, res) => {
+	res.render('addAdventure')
+})
+
+router.post('/', async (req, res) => {
+	console.log('post')
 })
 
 module.exports = router
