e39320ec507dd9e29a65edf97ecbcf910dfe0d91
Puzant <PBAKJEJIAN@hotmail.com>
2024-01-26 15:41:28 +0200

    :sparkles: add Delete, Add routes

--- routes/adventures.js
+++ routes/adventures.js
@@ -3,33 +3,65 @@ const Adventure = require('../models/adventure');
 
 const router = express.Router()
 
-const mockdata = [
-	{
-		date: '2022-9/2',
-		duration: '16',
-		country: 'Armenia',
-		city: 'Yerevan',
-		travelStyle: 'Adventure Travel'
-	},
-	{
-		date: '2015-5/10',
-		duration: '10',
-		country: 'Lebanon',
-		city: 'Beirut',
-		travelStyle: 'Adventure Travel'
-	},
-]
-
 router.get('/', async (req, res) => {
-	res.render('index', { adventures: mockdata });
+	try {
+		const adventures = await Adventure.find()
+		res.render('index', { adventures: adventures });
+	} catch(err) {
+		console.error(err);
+    res.status(500).send('Internal Server Error');	
+	}
 })
 
-router.get('/add-adventure', async (req, res) => {
+router.get('/add-adventure', (req, res) => {
 	res.render('addAdventure')
 })
 
-router.post('/', async (req, res) => {
-	console.log('post')
+router.get('/adventure-details/:id', async (req, res) => {
+	const adventureId = req.params.id
+	const adventure = await Adventure.findById(adventureId)
+	console.log(adventure, 'this is adventure')
+
+	if (!adventure) {
+		return res.status(404).send('Adventure not found');
+	}
+
+	res.render('adventureDetails', { adventure })
+})
+
+router.post('/add-adventure', async (req, res) => {
+	try {
+		const { country, city, date, duration, travelStyle } = req.body;
+
+		const newAdventure = new Adventure({
+      country,
+      city,
+      date,
+      duration,
+      travelStyle,
+    })
+
+		await newAdventure.save();
+		res.redirect('/add-adventure');
+	} catch (err) {
+		console.error(err);
+    res.status(500).send('Internal Server Error');
+	}
+})
+
+router.delete('/adventures/:id', async (req, res) => {
+	const adventureId = req.params.id
+
+	try {
+		const adventure = await Adventure.findByIdAndDelete(adventureId)
+		if (!adventureId) 
+			return res.status(404).send('Adventure not found');
+
+		res.status(200).json({ message: 'Adventure deleted successfully', deletedAdventure: adventure });
+	} catch (err) {
+		console.error(err);
+    res.status(500).send('Internal Server Error');
+	}
 })
 
 module.exports = router
